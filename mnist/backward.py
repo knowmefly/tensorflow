6241d80e8dccbcb5b7a20b99a045d2a33ff00d56
knowmefly <knowmefly@users.noreply.github.com>
2018-05-10 09:08:51 +0800

    up

--- mnist/backward.py
+++ mnist/backward.py
@@ -14,8 +14,8 @@ LEARNING_RATE_BASE = 0.1
 LEARNING_RATE_DECAY = 0.99
 REGULARIZER = 0.0001
 STEPS = 50000
-MOVING_AVERAFE_DECAY = 0.99
-MODEL_SAVE_PATG="./model/"
+MOVING_AVERAGE_DECAY = 0.99
+MODEL_SAVE_PATH="./model/"
 MODEL_NAME="mnist_model"
 
 def backward(mnist):
@@ -40,7 +40,7 @@ def backward(mnist):
 		staircase=True)
 	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
 	#滑动平均
-	ema = tf.train.ExponentialMovingAverage(MOVING_AVERAFE_DECAY, global_step)
+	ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
 	ema_op = ema.apply(tf.trainable_variables())
 	with tf.control_dependencies([train_step, ema_op]):
 		train_op = tf.no_op(name='train')
@@ -52,15 +52,16 @@ def backward(mnist):
 		init_op = tf.global_variables_initializer()
 		sess.run(init_op)
 		# tf.initialize_all_variables().run()
-
+		#ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
+		#global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
 		#训练模型
-		for i in range(STEPS):
+		for i in range(STEPS):			
 			xs, ys = mnist.train.next_batch(BATCH_SIZE)
 			_, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
 			# sess.run(train_step, feed_dict={x: , y_: })
 			if i % 1000 == 0:
 				print("After %d training step(s), loss on training batch is %f." %(step, loss_value))
-				saver.save(sess, os.path.join(MODEL_SAVE_PATG, MODEL_NAME), global_step=global_step)
+				saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
 
 def main():
 	mnist = input_data.read_data_sets("./data/", one_hot=True)
