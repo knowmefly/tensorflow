da35c9d2dc37401f2fbad4580bdb722d2585dc69
knowmefly <knowmefly@qq.com>
2018-05-30 21:29:28 +0800

    'up'

---
+++ fc4/backward.py
@@ -0,0 +1,83 @@
+#coding:utf-8
+
+#导入模块
+import tensorflow as tf 
+from tensorflow.examples.tutorials.mnist import input_data
+import forward
+import os
+import generateds#1
+import win_unicode_console
+win_unicode_console.enable()
+
+
+BATCH_SIZE = 200
+LEARNING_RATE_BASE = 0.1
+LEARNING_RATE_DECAY = 0.99
+REGULARIZER = 0.0001
+STEPS = 50000
+MOVING_AVERAGE_DECAY = 0.99
+MODEL_SAVE_PATH="./model/"
+MODEL_NAME="mnist_model"
+train_num_examples = 60000#2 mnist_train-num_examples
+
+def backward(mnist):
+	x = tf.placeholder(tf.float32, [None, forward.INPUT_NODE])
+	y_ = tf.placeholder(tf.float32, [None, forward.OUTPUT_NODE])
+
+	#定义前向传播函数
+	y = forward.forward(x, REGULARIZER)
+	global_step = tf.Variable(0, trainable=False)
+	# loss = 
+	
+	
+	ce = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1))
+	cem = tf.reduce_mean(ce)
+	loss = cem + tf.add_n(tf.get_collection('losses'))
+	#指数衰减学习率
+	learning_rate = tf.train.exponential_decay(
+		LEARNING_RATE_BASE,
+		global_step,
+		mnist.train.num_examples / BATCH_SIZE,
+		LEARNING_RATE_DECAY,
+		staircase=True)
+	train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+	#滑动平均
+	ema = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+	ema_op = ema.apply(tf.trainable_variables())
+	with tf.control_dependencies([train_step, ema_op]):
+		train_op = tf.no_op(name='train')
+	
+	#实例化saver对象
+	saver = tf.train.Saver()
+
+	img_batch, label_batch = generateds.get_tfrecord(BATCH_SIZE, isTrain=True)#3
+
+	with tf.Session() as sess:
+		#初始化所有模型参数
+		init_op = tf.global_variables_initializer()
+		sess.run(init_op)
+		# tf.initialize_all_variables().run()
+		ckpt = tf.train.get_checkpoint_state(MODEL_SAVE_PATH)
+		if ckpt and ckpt.model_checkpoint_path:
+			saver.restore(sess, ckpt.model_checkpoint_path)
+
+		coord = tf.train.Coordinator()#4
+		threads = tf.train.start_queue_runners(sess=sess, coord=coord)#5
+		#global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+		#训练模型
+		for i in range(STEPS):			
+			xs, ys = sess.run([img_batch, label_batch]) #6 xs,ys=train.next_batch(BATCH_SIZE)
+			_, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: xs, y_: ys})
+			# sess.run(train_step, feed_dict={x: , y_: })
+			if i % 1000 == 0:
+				print("After %d training step(s), loss on training batch is %f." %(step, loss_value))
+				saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+		coord.request_stop()#7
+		coord.join(threads)#8
+def main():
+	mnist = input_data.read_data_sets("./data/", one_hot=True)
+	backward(mnist)
+
+if __name__ == '__main__':
+	main()
---
+++ fc4/test.py
@@ -0,0 +1,67 @@
+#coding:utf-8
+
+#导入模块
+import tensorflow as tf 
+import time
+from tensorflow.examples.tutorials.mnist import input_data
+import forward
+import backward
+import generateds
+import win_unicode_console
+win_unicode_console.enable()
+
+TEST_INTERVAL_SECS = 5
+REGULARIZER = 0.0001
+TEST_NUM = 10000#1 
+def test(mnist):
+	with tf.Graph().as_default() as g:
+		#x y_占位
+		x = tf.placeholder(tf.float32, [None, forward.INPUT_NODE])
+		y_ = tf.placeholder(tf.float32, [None, forward.OUTPUT_NODE])
+
+		#前向传播预测结果y
+		y = forward.forward(x, REGULARIZER)
+
+		ema = tf.train.ExponentialMovingAverage(backward.MOVING_AVERAGE_DECAY)
+		ema_restore = ema.variables_to_restore()
+		saver = tf.train.Saver(ema_restore)
+
+		#计算正确率
+		correct_prediction = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+		accuracy = tf.reduce_mean(tf.cast(correct_prediction,tf.float32))
+
+		img_batch, label_batch = generateds.get_tfrecord(BATCH_SIZE, isTrain=False)#2
+		while True:
+			with tf.Session() as sess:
+				#加载训练好的模型
+				ckpt = tf.train.get_checkpoint_state(backward.MODEL_SAVE_PATH)
+				#如果已有ckpt则恢复
+				if ckpt and ckpt.model_checkpoint_path:
+					#恢复会话
+					saver.restore(sess, ckpt.model_checkpoint_path)
+					#恢复轮数
+					global_step = ckpt.model_checkpoint_path.split('/')[-1].split('-')[-1]
+
+					coord = tf.train.Coordinator()#3
+					threads = tf.train.start_queue_runners(sess=sess, coord=coord)#4
+					xs, ys = sess.run([img_batch, label_batch])#5
+
+					#计算准确率
+					accuracy_score = sess.run(accuracy, feed_dict={x:mnist.test.images, y_:mnist.test.labels})
+					print("After %s training step(s), test accuracy = %g" %(global_step, accuracy_score))
+					
+					coord.request_stop()#6
+					coord.join(threads)
+				#如果没有模型
+				else:
+					print('No checkpoint file found') #模型不存在
+					return 
+				time.sleep(TEST_INTERVAL_SECS)
+def main():
+	#加载测试数据集
+	mnist = input_data.read_data_sets("./data/", one_hot=True)
+	#调用test（）函数
+	test(mnist)
+
+if __name__=='__main__':
+	main()
