c43d6709c8653b28c11ec5bd2963eecf45236a5d
knowmefly <knowmefly@qq.com>
2019-02-18 19:28:59 +0800

    sync

---
+++ example/retrain.sh
@@ -0,0 +1,1 @@
+python3 retrain.py --bottleneck_dir model/bottleneck --image_dir pic/ --how_many_training_steps 200 --model_dir model/inception_model/ --output_graph model/output_graph.pb --output_labels model/output_labels.txt
